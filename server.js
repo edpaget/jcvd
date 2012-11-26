770cfb1a9101993cb2bc819865edcfe90e3499e6
Ed Paget <ed.paget@gmail.com>
2012-11-26 12:16:47 -0600

    Add saving csv to redis and retrieving via get request

--- server.js
+++ server.js
@@ -10,19 +10,49 @@ server.listen(port)
 
 console.log("Server started at: ", port);
 
-app.options("*", function(req, res) {
+if ( port === 3001 ) {
+  var redis = require('redis').createClient();
+  console.log("Connected to Local Redis");
+} else {
+  var redisUrl = require('url').parse(process.env.REDISTOGO_URL);
+  var redis = require('redis').createClient(redisUrl.port, redisUrl.hostname);
+  redis.auth(redisUrl.auth.split(":")[1]);
+  console.log("Connected to RegisToGo instance");
+}
+
+var corsHeaders = function(res) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+};
+
+app.options("*", function(req, res) {
+  corsHeaders(res);
   res.send();
 });
 
 app.post('/to-csv', function(req, res) {
+  key = Math.floor((Math.random() * 100000) + 1);
+  redis.set(key, toCSV(req.body));
 
-  console.log(toCSV(req.body));
+  body = JSON.stringify({ data_url: key }); 
+  corsHeaders(res);
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Conent-Length', body.length);
+  res.send(body);
+});
 
-  res.send('THANKS!');
+app.get('/to-csv/:id', function(req, res) {
+  var id = req.params.id
+  csv = redis.get(id, function(err, reply) {
+    redis.del(id);
+    corsHeaders(res);
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Content-Disposition', 'attachment;filename=navigator_data.csv');
+    res.setHeader('Content-Length', reply.length);
+    res.send(reply);
+  });
 });
 
 var toCSV = function(data) {
